a651dd9280d536f6c61e4a1f23a65ba7f31f7f40
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2010-08-24 19:32:22 +0200

    hello world

---
+++ application.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  <<-EOS
+  - Znaleźć dobrą nazwę dla wyszukiwarki...- bagatela... 
+  - Jest! - zawołać miał na to Dąbrowski - "BAGATELA"!!!
+  EOS
+end
