f031dc13bb303129b10a43006a787d86e8acc406
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2010-09-09 22:59:47 +0200

    added and modified rspec tests

--- spec/seach_spec.rb
+++ spec/seach_spec.rb
@@ -9,7 +9,7 @@ describe 'search engine' do
 
   it "should not find random stop" do
 
-    Stop.new :name => "Beginning"
+    Hub.new :name => "Beginning"
     Neo4j::Transaction.finish
 
     post '/Beginning:Random'
@@ -23,7 +23,7 @@ describe 'search engine' do
 
   it "should be cool if beginning is ending" do
 
-    Stop.new :name => "Beginning"
+    Hub.new :name => "Beginning"
     Neo4j::Transaction.finish
 
     post '/Beginning:Beginning'
@@ -37,45 +37,42 @@ describe 'search engine' do
 
   it "should apologize if there is not connection" do
 
-    Stop.new :name => "Beginning"
-    Stop.new :name => "Nowhere"
+    Hub.new :name => "Beginning", :lat => 0, :lon => 0
+    Hub.new :name => "Nowhere",   :lat => 1, :lon => 1
     Neo4j::Transaction.finish
 
     post '/Beginning:Nowhere'
-    last_response.ok?.should be_true
+    last_response.should be_ok
     last_response.body.should be_json
 
     body = JSON.parse(last_response.body)
-    body['error'].should eql("not_found")
-    body['reason'].should eql("no_connection")
+    body['from'].should eql("Beginning")
+    body['to'].should eql("Nowhere")
+    body['results'].should be_empty
   end
 
   it "should find direct connection between two stops" do
 
-    s1 = Stop.new :name => "Beginning"
-    s2 = Stop.new :name => "Ending"
+    h1 = Hub.new :name => 'Beginning', :lat => 0, :lon => 0
+    h2 = Hub.new :name => 'Ending',    :lat => 1, :lon => 1
 
-    s1.rels.outgoing(:connections) << s2
-
-    r1 = Run.new
-    r1.time = [12,0] # 12:00
-    r2 = Run.new
-    r2.time = [12,2] # 12:02
-
-    r1.rels.outgoing(:connection) << r2
-    s1.rels.outgoing(:departures) << r1
-    s2.rels.outgoing(:departures) << r2
+    direction = h1.connects.new h2
+    direction.timetables = {
+      12*60     => 2, # departures at 12:00 and arrives two minutes later
+      12*60 + 3 => 2, #               12:03
+      12*60 + 5 => 2  #               12:05
+    }.to_json
 
     Neo4j::Transaction.finish
 
-    post '/Beginning:Ending'
-    last_response.ok?.should be_true
+    post '/Beginning:Ending', {'time'=>'11:59'}
+    last_response.should be_ok
     last_response.body.should be_json
 
     body = JSON.parse(last_response.body)
     body['from'].should eql("Beginning")
     body['to'].should eql("Ending")
-    body['duration'].should eql(2)
+    body['duration'].should eql(3)
 
     body['results'].should have(2).stops
     body['results'][0].should eql({
@@ -88,7 +85,142 @@ describe 'search engine' do
     })
   end
 
-  it "should find connection with transfer between two stops"
+  it "should find connection with transfer between two stops" do
 
-  it "should find the fastest connection between two stops"
+    h1 = Hub.new :name => 'Begin', :lat => 0, :lon => 0
+    h2 = Hub.new :name => 'Transfer', :lat => 1, :lon => 1
+    h3 = Hub.new :name => 'End', :lat => 2, :lon => 2
+
+
+    h1.connects.new(h2).timetables = {
+      12*60     => 2, # departures at 12:00 and arrives two minutes later
+    }.to_json
+
+    h2.connects.new(h3).timetables = {
+      12*60     => 2, # departures at 12:00 and arrives two minutes later
+      12*60 + 5 => 2  #               12:05
+    }.to_json
+
+    Neo4j::Transaction.finish
+
+    post '/Begin:End', {'time'=>'11:55'}
+    last_response.should be_ok
+    last_response.body.should be_json
+
+    body = JSON.parse(last_response.body)
+    body['duration'].should eql(12)
+
+    body['results'].should have(3).stops
+    body['results'][1].should eql({
+      "arrival"   => "12:02",
+      "departure" => "12:05",
+      "stop"      => "Transfer"
+    })
+    body['results'][2].should eql({
+      "arrival" => "12:07",
+      "stop"    => "End"
+    })
+  end
+
+  describe "heuristics" do
+
+    # Given is the following graph:
+    # 1_____2
+    # .\....|
+    # ..3...|
+    # ..|\..|
+    # ..\.4.|
+    # ...\.\|
+    # ....5_6
+
+    before :each do
+      h1 = Hub.new :name => 'Hub1', :lat => 0, :lon => 0
+      h2 = Hub.new :name => 'Hub2', :lat => 6, :lon => 0
+      h3 = Hub.new :name => 'Hub3', :lat => 2, :lon => 2
+      h4 = Hub.new :name => 'Hub4', :lat => 4, :lon => 4
+      h5 = Hub.new :name => 'hub5', :lat => 4, :lon => 6
+      h6 = Hub.new :name => 'hub6', :lat => 6, :lon => 6
+
+      direction = h1.connects.new h2
+      direction.timetables = {
+        12*60     => 2, # departures at 12:00 and arrives two minutes later
+        12*60 + 3 => 2, #               12:03
+        12*60 + 5 => 2  #               12:05
+      }.to_json
+
+      direction = h1.connects.new h3
+      direction.timetables = {
+        12*60 + 1 => 2, # departures at 12:01 and arrives two minutes later
+        12*60 + 4 => 2, #               12:04
+        12*60 + 7 => 2  #               12:07
+      }.to_json
+
+      direction = h2.connects.new h6
+      direction.timetables = {
+        12*60 + 2 => 2, # departures at 12:02 and arrives two minutes later
+        12*60 + 5 => 2, #               12:05
+        12*60 + 7 => 2  #               12:07
+      }.to_json
+
+      direction = h3.connects.new h4
+      direction.timetables = {
+        12*60 + 6 => 2, #               12:06
+        12*60 + 9 => 2  #               12:09
+      }.to_json
+
+      direction = h3.connects.new h5
+      direction.timetables = {
+        12*60 + 3 => 2, # departures at 12:03 and arrives two minutes later
+      }.to_json
+
+      direction = h4.connects.new h6
+      direction.timetables = {
+        12*60 + 8 => 2, # departures at 12:08 and arrives two minutes later
+        12*60 +11 => 2, #               12:11
+      }.to_json
+
+      direction = h5.connects.new h6
+      direction.timetables = {
+        12*60 + 5 => 2, # departures at 12:05 and arrives two minutes later
+      }.to_json
+
+      Neo4j::Transaction.finish
+    end
+  
+    it "should find the shortest path between two stops" do
+
+      post '/Hub1:Hub6', {'time'=>'11:59'}
+      last_response.should be_ok
+      last_response.body.should be_json
+
+      body = JSON.parse(last_response.body)
+      body['duration'].should eql(6)
+      body['distance'].should be_close(GeoCostEvaluator.distance(0,0,6,6), 1)
+
+      body['results'].should have(4).stops
+      body['results'].last.should eql({
+        "arrival" => "12:13",
+        "stop"    => "Hub6"
+      })
+     
+    end
+
+    it "should find the fastest connection between two stops" do
+
+      post '/Hub1:Hub6', {'time'=>'11:59', 'priority'=>'time'}
+      last_response.should be_ok
+      last_response.body.should be_json
+
+      body = JSON.parse(last_response.body)
+      body['duration'].should eql(6)
+      body['distance'].should be_close(GeoCostEvaluator.distance(0,0,6,0) + GeoCostEvaluator.distance(6,0,6,6), 1)
+
+      body['results'].should have(3).stops
+      body['results'].last.should eql({
+        "arrival" => "12:04",
+        "stop"    => "Hub6"
+      })
+    end
+
+  end
 end
