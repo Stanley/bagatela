603615ad9a72b05b667537e8547a628b1d1f1101
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2011-06-29 13:07:59 +0200

    + config files

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,13 @@
 require 'rubygems'
-require 'json'
-require 'neo4j'
-require 'picky-client'
-require 'rest_client'
+#require 'neo4j'
+require 'yaml'
+require 'yajl/json_gem'
 require File.join(File.dirname(__FILE__), 'exceptions')
 
-# Set up query instance
-Stops = Picky::Client::Full.new :host => 'localhost', :port => 8080, :path => '/stops/full'
-Nodes = Picky::Client::Full.new :host => 'localhost', :port => 8080, :path => '/nodes/full'
-Couch = RestClient::Resource.new 'http://localhost:5984/stops'
+#$: << File.join(File.dirname(__FILE__), '..', 'app', 'models')
+#require 'connection'
+#require 'hub'
+#require 'a_star'
 
-$: << File.join(File.dirname(__FILE__), '..', 'app', 'models')
-require 'connection'
-require 'hub'
-require 'a_star'
+config = YAML::load File.read File.join(File.dirname(__FILE__), 'database.yaml')
+CONFIG = config[ENV['RACK_ENV'] || "development"]
