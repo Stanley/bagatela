03930c8ba7abca9c96d01a34ea0287f3057fa1a1
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2011-11-11 22:47:57 +0000

    - import graph from cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,9 +4,7 @@ every :day, :at=>'0 am' do
   cities = "#{root}/config/cities/"
   Dir.foreach(cities) do |conf|
     next unless match = conf.match(/([a-z]+)\.json$/) and code = match[1]
-    #json = JSON.parse(File.read(file))
-    #couchdb = couch + json['code']
-
+    
     # Create database if it doesn't exists, create or update design documents
     # and import stops.
     rake "couchdb[#{code}]"
@@ -14,8 +12,5 @@ every :day, :at=>'0 am' do
     # Import timetables.
     pigeons = File.join(root,'bin','import','timetables','pigeons.js')
     command "node --use-http2 #{pigeons} #{cities+conf}"
-
-    # Create city graph based on imported timetables.
-    runner "Bagatela::Graph::Import.relationships!('#{code}', true)"
   end
 end
